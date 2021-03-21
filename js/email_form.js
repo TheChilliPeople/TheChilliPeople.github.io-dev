176e87721934009266953e1671d355ab07765b67
Matt Cockerill <matt.cockerill1@gmail.com>
2021-03-21 16:35:27 +0000

    form changes

--- js/email_form.js
+++ js/email_form.js
@@ -1,22 +1,16 @@
-var $form = $('form#email-form'),
+var $form = $('form#email_form'),
     url = 'https://script.google.com/macros/s/AKfycbzFTgPRJ8XBpg_TjJdztK_80QDJbC9vFTMV62A5W7PoWdS8HHgUK-h8LFBT_U-9qF55/exec'
 
-$('#submit-form').on('click', function(e) {
+$('#form_submit').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
     data: $form.serializeObject()
-  }).success(
-    // do something
+  }).success( function(result) {
+      $("form.email_form").css('display', 'none'); //hide form
+      alert('Thank you for your Contacting us'); //show thank you message
+    } 
   );
 })
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  let form_response = document.querySelector('#form_response');
-  let form_submit = document.querySelector('#submit_form');
-  form_submit.onclick = function() {
-      form_response.innerHTML = 'Thanks!';
-  }; })
